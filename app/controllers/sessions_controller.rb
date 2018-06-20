c2a55d7968d067b801136e5c4def0bcfb7a1258e
nguyenhongphuc2018 <nguyen.hong.phuc@framgia.com>
2018-06-20 15:16:05 +0700

    Show personal information customer

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
     user = User.find_by email: params[:session][:email].downcase
     if user && user.authenticate(params[:session][:password])
       log_in user
-      user.admin? ? redirect_to(admin_url) : redirect_to(user)
+      user.admin? ? redirect_to(admin_url) : redirect_to(root_url)
     else
       flash.now[:danger] = t "invalid"
       render :new
